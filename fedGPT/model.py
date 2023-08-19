02b5a3fc487329cf77542cdd86311cb6b62173d2
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2023-08-19 21:01:47 +0530

    nit

--- fedGPT/model.py
+++ fedGPT/model.py
@@ -5,16 +5,6 @@ import torch
 import torch.nn as nn
 
 
-# --------------------------------- Attention -------------------------------- #
-class Attention(nn.Module):
-    def __init__(self, n_embed: int, head_size: int) -> None:
-        super().__init__()
-        self.Q = nn.Linear(n_embed, head_size, bias=False)
-        self.K = nn.Linear(n_embed, head_size, bias=False)
-        self.V = nn.Linear(n_embed, head_size, bias=False)
-
-        tril = torch.tril()
-
 class nanoGPT(nn.Module):
     pass
 
